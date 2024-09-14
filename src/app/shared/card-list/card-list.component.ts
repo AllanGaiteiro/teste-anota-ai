5aa81cb09324d2d85b17acdaea55f4593c989682
Allan <gaiteirodeveloper+github@gmail.com>
2024-09-14 17:46:04 -0300

    feat: implement service and card list, create methods by filter

---
+++ src/app/shared/card-list/card-list.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { CardItemComponent } from '../card-item/card-item.component';
+import { ItemEntity } from '../../core/entities/item.entity';
+import { ItemService } from '../../core/services/item.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-card-list',
+  standalone: true,
+  imports: [CommonModule, CardItemComponent],
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.Default,
+})
+export class CardListComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() searchText: string = '';
+  itens: ItemEntity[] = [];
+  filteredItems: ItemEntity[] = [];
+  itensSubscription?: Subscription;
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.itensSubscription = this.itemService.findItens().subscribe({
+      next: (res) => {
+        this.itens = res;
+        this.filterItems();
+      },
+      error: (err) => console.error(err),
+    })
+  }
+
+  ngOnChanges(): void {
+    this.filterItems();
+  }
+
+  ngOnDestroy(): void {
+    if (this.itensSubscription) this.itensSubscription.unsubscribe();
+  }
+
+  filterItems(): void {
+    if (!this.searchText) {
+      this.filteredItems = this.itens;
+    } else {
+      this.filteredItems = this.itens.filter(item =>
+        item.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        item.description.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    }
+  }
+}
