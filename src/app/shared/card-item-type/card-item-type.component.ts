08e9fbc4ba6df060ad2e47542a6276cd26e94d4d
Allan <gaiteirodeveloper+github@gmail.com>
2024-09-14 17:43:27 -0300

    feat: set card item type, add style and type names

--- src/app/shared/card-item-type/card-item-type.component.ts
+++ src/app/shared/card-item-type/card-item-type.component.ts
@@ -7,10 +7,25 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   imports: [
     CommonModule,
   ],
-  template: `<p *ngIf="type" class="card-item-type">{{type}}</p>`,
+  template: `<p *ngIf="getTypeName()" [class]="getTypeClass()">{{getTypeName()}}</p>`,
   styleUrl: './card-item-type.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardItemTypeComponent {
   @Input() type?: string;
+
+  typeNames = [
+    { id: '1', name: 'Paisagem' },
+    { id: '2', name: 'Flor' },
+    { id: '3', name: 'Pizza' },
+  ];
+
+  getTypeName(): string | null {
+    return this.typeNames.find(t => t.id === this.type)?.name || null;
+  }
+  getTypeClass(): string {
+    const typeObj = this.typeNames.find(t => t.id === this.type);
+    if (typeObj) return `card-item-type-${typeObj.id}`;
+    return 'card-item-type';
+  }
 }
